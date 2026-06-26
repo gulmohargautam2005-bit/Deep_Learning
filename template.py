ab81f337ddf97170e78c36ed014dfb1b180eec2f
gulmohargautam2005-bit <gulmohargautam2005@gmail.com>
2026-06-26 11:28:12 +0530

    folder structure added

---
+++ template.py
@@ -0,0 +1,11 @@
+import os
+from pathlib import Path
+import logging
+
+# logging string
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(asctime)s]: %(message)s:'
+)
+
+project_nme ='CNN_Classifier'
---
+++ template.sh
@@ -0,0 +1,24 @@
+
+mkdir -p research
+mkdir -p src/utils src/components src/config src/pipeline src/entity src/constants config templates
+
+touch src/utils/__init__.py
+touch src/components/__init__.py
+touch src/config/__init__.py
+touch src/config/configuration.py
+touch src/pipeline/__init__.py
+touch src/entity/__init__.py
+touch src/constants/__init__.py
+
+touch config/config.yaml
+touch requirements.txt
+touch setup.py
+touch research/trials.ipynb
+touch templates/index.html
+touch app.py
+touch params.yaml
+touch dvc.yaml
+
+
+
+
